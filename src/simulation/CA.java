05fbeeac17c9ac3a02fe8b6a76fe264a1d90f66a
JULIA SAVELIFF <JMS247@duke.edu>
2018-09-17 19:54:29 -0400

    Created main class and module

---
+++ src/simulation/CA.java
@@ -0,0 +1,19 @@
+package simulation;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CA extends Application {
+
+    @Override
+    public void start(Stage stage) {
+    }
+
+    /**
+     * Start the program.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
