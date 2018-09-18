1127857f07dce2dedcc27dd883dabdf92047cf98
Haotian Wang <haotian.wang@duke.edu>
2018-09-18 00:57:22 -0400

    Modified module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,7 @@
-module cellsociety_team13 {
+module cellsociety_team13 {im not sure.. another data structure?
+list of lists?
+not sure how that would be better
+also maybe don't add these messages verbatim to the doc because it's mostly "my ta said this.." lol
         requires javafx.base;
         requires javafx.controls;
         requires javafx.graphics;
