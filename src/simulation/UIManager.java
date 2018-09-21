5001d325b034ef1d92bf0115cf3aeda83a3004c4
JULIA SAVELIFF <JMS247@duke.edu>
2018-09-21 17:24:22 -0400

    Added grid and step update to UI

--- src/simulation/UIManager.java
+++ src/simulation/UIManager.java
@@ -7,6 +7,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
@@ -17,6 +18,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 
 public class UIManager {
     // constant screen dimensions
@@ -26,6 +28,7 @@ public class UIManager {
 
     // button text
     private final static String LOAD_NEW_TEXT = "Load new simulation";
+    private final static String RESET_TEXT = "Reset";
     private final static String PAUSE_TEXT = "Pause";
     private final static String RESUME_TEXT = "Resume";
     private final static String STEP_TEXT = "Step";
@@ -48,6 +51,10 @@ public class UIManager {
     private Button splashToSimButton;
     private Button simToSplashButton;
     private Slider mySpeedSlider;
+    private Grid myGrid;
+    private Rule myRule;
+    private int myNumGenerations=1;
+    private Text myGenerationsDisplay;
 
     // file read components
     private File myFile;
@@ -96,6 +103,16 @@ public class UIManager {
 
         // Move test text across screen
         textToTestStep.setX(textToTestStep.getX()-50*stepTime);
+
+        // Update num generations
+        myNumGenerations += 1;
+        myGenerationsDisplay.setText("Generations: "+myNumGenerations);
+        System.out.println(myNumGenerations);
+
+        //update cells
+        myRule.determineNextStates();
+        myRule.updateGrid();
+
     }
 
     private Group setupSplash(Stage stage) {
@@ -134,7 +151,6 @@ public class UIManager {
 
             try {
                 myReader = new ReadXML(myFile);
-                System.out.println("Created reader");
                 handleReader(myReader);
             } catch (ParserConfigurationException PCe) {
                 // TODO: handle exception
@@ -164,9 +180,12 @@ public class UIManager {
         userPanel.setPrefSize(PANEL_WIDTH,height);
         userPanel.setStyle("-fx-background-color: #336699");
         userPanel.setAlignment(Pos.CENTER);
-        simToSplashButton = new Button(LOAD_NEW_TEXT);
 
         // user simulation controls
+        myGenerationsDisplay = new Text("Generations: "+myNumGenerations);
+        simToSplashButton = new Button(LOAD_NEW_TEXT);
+        var resetButton = new Button(RESET_TEXT);
+        resetButton.setOnAction( event -> handleReset() );
         var pauseButton = new Button(PAUSE_TEXT);
         pauseButton.setOnAction( event -> handlePause() );
         var resumeButton = new Button(RESUME_TEXT);
@@ -184,13 +203,19 @@ public class UIManager {
         gridRegion.setPrefSize(width, height);
         var gridButton = new Button(title);
 
+        myGrid = new Grid(root, myReader);
+        myGrid.populateCells();
+        var cellsToAdd = myGrid.getAllImageView();
+
+        myRule = new GameOfLifeRule(myGrid);
+
         textToTestStep = new Text("hi...");
         textToTestStep.setX(300);
         textToTestStep.setY(150);
 
         // add elements to each region
-        userPanel.getChildren().addAll(simToSplashButton, pauseButton, resumeButton, stepButton, speedText, mySpeedSlider);
-        gridRegion.getChildren().add(gridButton);
+        userPanel.getChildren().addAll(myGenerationsDisplay, simToSplashButton, resetButton, pauseButton, resumeButton, stepButton, speedText, mySpeedSlider);
+        gridRegion.getChildren().addAll(cellsToAdd);
         gridRegion.getChildren().add(textToTestStep);
 
         // set layout regions
@@ -206,19 +231,18 @@ public class UIManager {
 
     private void setupSceneTransitions(Stage stage, Group splash, Group simulation) {
 
-        splashToSimButton.setOnAction( event -> { if(myFile !=null) {
-                    stage.setScene(simulation.getScene());
-                    run();
-                }
+        splashToSimButton.setOnAction( event -> {
+            if(myFile !=null) {
+                stage.setScene(simulation.getScene());
+                run();
             }
-        );
+        });
         simToSplashButton.setOnAction( event -> stage.setScene(splash.getScene()) );
 
     }
 
     private void handleReader(ReadXML reader) {
 
-        System.out.println("handleReader called");
         mySimRoot = setupSimulation(myStage, reader.getHeight(), reader.getWidth(), reader.getName());
         setupSceneTransitions(myStage, mySplashRoot, mySimRoot);
 
@@ -227,20 +251,21 @@ public class UIManager {
     private void handlePause() {
         // pause animation
         myAnimation.pause();
-        System.out.println(PAUSE_TEXT);
     }
 
     private void handleResume() {
         // resume animation
         myAnimation.play();
-        System.out.println(RESUME_TEXT);
     }
 
     private void handleStep() {
         // step through animation
         myAnimation.pause();
         step(SECOND_DELAY);
-        System.out.println(STEP_TEXT);
+    }
+
+    private void handleReset() {
+        myAnimation.playFromStart();
     }
 
     public File getFile() { return myFile; }
