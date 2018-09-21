a9330d74e8234b244e65bc01d5123b86681f329d
Yunhao Qing <yunhao.qing@duke.edu>
2018-09-21 14:31:21 -0400

    Beautify code.

--- src/simulation/SpreadingOfFireRule.java
+++ src/simulation/SpreadingOfFireRule.java
@@ -3,14 +3,12 @@ package simulation;
 import java.util.List;
 
 /**
- * A specific Rule class for GameOfLife game.
+ * A specific Rule class for SpreadingOfFire game.
  */
 public class SpreadingOfFireRule extends Rule {
-
-    public GameOfLifeRule(Grid grid) {
+    public SpreadingOfFireRule(Grid grid) {
         super(grid);
     }
-
     /**
      * Calculate the next state of all the cells in the grid, according to this article, http://nifty.stanford.edu/2007/shiflet-fire/.
      */
@@ -18,32 +16,31 @@ public class SpreadingOfFireRule extends Rule {
     public void determineNextStates() {
         List<Cell> FireCell = new ArrayList<Cell>();
         List<Cell> EmptyCellToFire = new ArrayList<Cell>();
-    
         for (int i = 0; i < this.getGrid().getNumRow(); i++) {
             for (int j = 0; j < this.getGrid().getNumCol(); j++) {
                 Cell cell = this.getGrid().item(i, j);
-                if (cell.getState() ==SpreadingOfFireCell.BURNING){
+                if (cell.getState() ==SpreadingOfFireCell.BURNING)
                     FireCell.add(cell);
-                }
                 if (cell.getState() == SpreadingOfFireCell.NORMAL){
                     boolean check = false;
                     List<Cell> neighbors = cell.getNeighbors(i, j);
                     for (Cell neighbor : neighbors) {
-                        if (neighbor != null && neighbor.getState() == SpreadingOfFireCell.BURNING) { check = true; }
-                }
+					if (neighbor != null && neighbor.getState() == SpreadingOfFireCell.BURNING){
+						check = true;
+						}
+					}
                  if (check == true){
-                 Random rand = new Random();
-                 boolean val = rand.nextInt(1/probCatch)==0;
-                 if (val == true)
-                 EmptyCellToFire.add(cell);
-                 }   
+					Random rand = new Random();
+					boolean val = rand.nextInt(1/probCatch)==0;
+					if (val == true)
+						EmptyCellToFire.add(cell);
+					}   
                 }
             }
         }
         for (Cell cell : FireCell)
-        cell.setNextState(SpreadingOfFireCellCell.EMPTY);
+			cell.setNextState(SpreadingOfFireCellCell.EMPTY);
         for (Cell cell : EmptyCellToFire)
-        cell.setNextState(SpreadingOfFireCellCell.BURNING);
-        
+			cell.setNextState(SpreadingOfFireCellCell.BURNING);
     }
 }
