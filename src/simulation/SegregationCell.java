7e54d7b292b24c0486ca12abb2002da9a9ac21bf
Haotian Wang <haotian.wang@duke.edu>
2018-09-21 18:06:01 -0400

    Minor edits to SegregationCell.

--- src/simulation/SegregationCell.java
+++ src/simulation/SegregationCell.java
@@ -4,5 +4,8 @@ package simulation;
 /**
  * @author Haotian Wang
  */
-public class SegregationCell {
+public class SegregationCell extends Cell{
+    public static final double SATISFACTION_THRESHOLD = 0.3;
+    public static final int TYPE_A = 0;
+    public static final int
 }
