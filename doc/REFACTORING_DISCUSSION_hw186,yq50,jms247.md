01fb9123264e4a61a4c699981e32256c30e45b08
Haotian Wang <haotian.wang@duke.edu>
2018-09-27 17:00:26 -0400

    Added a USER_PANEL_ITEM_SPACING to replace the magic value that represents the spacing between user panel children

--- doc/REFACTORING_DISCUSSION_hw186,yq50,jms247.md
+++ doc/REFACTORING_DISCUSSION_hw186,yq50,jms247.md
@@ -9,6 +9,10 @@ Duplication Refactoring
 -----------------------
 * Merge getNeighbors into a new method in the superclass such that it can take an array of index and outputs the neighbors corresponding to the input array.
 
+Checklist Refactoring
+---------------------
+* Added a USER_PANEL_ITEM_SPACING to replace the magic value that represents the spacing between user panel children.
+
 
 Longest Method Refactoring
 --------------------------
--- src/simulation/UIManager.java
+++ src/simulation/UIManager.java
@@ -29,6 +29,7 @@ public class UIManager {
     private final static int SPLASH_SIZE = 300;
     private final static int PANEL_WIDTH = 175;
     private final static int LAYOUT_SPACING = 20;
+    private final static int USER_PANEL_ITEM_SPACING = 20;
 
     // button text
     private ResourceBundle myResources;
@@ -180,7 +181,7 @@ public class UIManager {
         var border = new BorderPane();
 
         // user panel
-        var userPanel = new VBox(20);
+        var userPanel = new VBox(USER_PANEL_ITEM_SPACING);
         userPanel.setPrefSize(PANEL_WIDTH,height);
         userPanel.setAlignment(Pos.CENTER);
 
