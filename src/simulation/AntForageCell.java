8fc1b53693a25cf29274a564a25dfa9a4851229a
Yunhao Qing <yunhao.qing@duke.edu>
2018-09-30 20:09:11 -0400

    Update AntForageCell.java

--- src/simulation/AntForageCell.java
+++ src/simulation/AntForageCell.java
@@ -20,43 +20,19 @@ public class AntForageCell extends Cell {
     public static final int EMPTY = 0;
     public static final int NEST = 1;
     public static final int FOOD = 2;
-    private static final Paint EMPTY_COLOR = Color.LIGHTGRAY;
-    private static final Paint NEST_COLOR = Color.GREEN;
-    private static final Paint FOOD_COLOR = Color.RED;
-    private static final Map<Integer, Paint> STATE_TO_PAINT_MAP = initMap();
     private double foodPheromones;
     private double nestPheromones;
-
-
-
-    /**
-     * @return An unmodifiable map.
-     */
-    private static Map<Integer, Paint> initMap() {
-        Map<Integer, Paint> map = new HashMap<>();
-        map.put(EMPTY, EMPTY_COLOR);
-        map.put(NEST, NEST_COLOR);
-        map.put(FOOD, FOOD_COLOR);
-        return Collections.unmodifiableMap(map);
-    }
 
     /**
      * Constructor for AntForageCell.
      */
-    public AntForageCell(double x, double y, double width, double height, int state) {
-        super(x, y, width, height, state);
-        this.setRectangle(STATE_TO_PAINT_MAP.get(state));
-        foodPheromones = 0.0;
-        nestPheromones = 0.0;
+    public AntForageCell(int state, int i, int j) {
+        super(state, i, j);
+        this.foodPheromones = 0.0;
+        this.nestPheromones = 0.0;
     }
 
-    /**
-     * Update the color of the shape of the cell to its corresponding next state.
-     */
-    @Override
-    protected void updateShape(Map<Integer, Paint> map) {
-        super.updateShape(AntForageCell.STATE_TO_PAINT_MAP);
-    }
+
 
     public double getFoodPheromones(){
         return foodPheromones;
